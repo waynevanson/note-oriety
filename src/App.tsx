1c7b0b0661faa18c3a1826237bfc823932cc4a9c
Wayne Van Son <waynevanson@gmail.com>
2024-12-19 21:45:48 +1100

    feat: add accuracy and outcome

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ButtonGrid } from "./components/button-grid"
 import { KEY_SIGNATURES, VexFrame } from "./components/vex-frame"
 import { NOTES_LABELS } from "./constants"
 import "./reset.css"
+import { createStore } from "solid-js/store"
 
 function createAnswer() {
   return Math.floor(Math.random() * NOTES_LABELS.length)
@@ -16,16 +17,28 @@ export function App() {
     createSignal<(typeof KEY_SIGNATURES)[number]>("C")
   const [streak, streakSet] = createSignal(0)
 
+  const [outcome, outcomeSet] = createStore({ incorrect: 0, correct: 0 })
+  const total = createMemo(() => outcome.correct + outcome.incorrect)
+
+  const accuracy = createMemo(() =>
+    total() <= 0 ? 0 : Math.trunc(100 * (outcome.correct / total()))
+  )
+
   const note = createMemo(() => NOTES_LABELS[answer()]!)
 
   function handleGuess(index: number) {
+    // wrong guess
     if (index !== answer()) {
       streakSet(0)
+      outcomeSet("incorrect", (a) => a + 1)
       return
     }
 
+    // correct guess
     streakSet((number) => number + 1)
+    outcomeSet("correct", (a) => a + 1)
 
+    // ensure answer isn't the same as before
     while (true) {
       const next = createAnswer()
       if (next === answer()) continue
@@ -53,22 +66,13 @@ export function App() {
               </option>
             ))}
           </select>
-          <span> Streak: {streak()}</span>
         </label>
+        <span> Streak: {streak()} </span>
+        <span>
+          Accuracy: {accuracy()}% ({outcome.correct} of {total()})
+        </span>
       </section>
       <ButtonGrid answer={answer} onClick={handleGuess} />
     </main>
   )
-}
-
-export function createAccuracy(corrects: Array<boolean>): number {
-  const total = corrects.length
-
-  if (total <= 0) return 0
-
-  const sum = corrects
-    .map((boolean) => Number(boolean))
-    .reduce((left, right) => left + right, 0)
-
-  return Math.trunc(100 * (sum / total))
 }
